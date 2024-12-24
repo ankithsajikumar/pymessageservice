206952d97fd3c8427e021c27dbdf24ffaa17ff3f
ankit <ankith.sajikumar@ihg.com>
2024-12-25 00:44:07 +0530

    admin tab updates

--- messagesApp/admin.py
+++ messagesApp/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 
 from .models import Message
 
+@admin.action(description="Mark selected messages as unread")
+def make_unread(modeladmin, request, queryset):
+    queryset.update(is_read=False)
+
 class MessageAdmin(admin.ModelAdmin):
+    ordering = ['timestamp']
     list_display = ["id", "topic", "text"]
     search_fields = ['topic', 'text']
     list_filter = ['topic']
+    actions = [make_unread]
 
 admin.site.register(Message, MessageAdmin)
--- smartDevices/apps.py
+++ smartDevices/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class SmartdevicesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'smartDevices'
-    verbose_name = "Devices"
+    verbose_name = "Smart Devices"
